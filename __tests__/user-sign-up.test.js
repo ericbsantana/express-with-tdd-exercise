68b19317527596852b9095df08527e3980cbdf2c
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-19 05:21:39 -0300

    test: create test to check if user is saved on database

--- __tests__/user-sign-up.test.js
+++ __tests__/user-sign-up.test.js
@@ -1,6 +1,17 @@
 const request = require("supertest")
 const app = require("../src/app")
 
+const User = require("../src/models/User")
+const sequelize = require("../src/config/database")
+
+beforeAll(() => {
+  return sequelize.sync()
+})
+
+beforeEach(() => {
+  return User.destroy({ truncate: true })
+})
+
 describe("user sign up", () => {
   it("should return 200 OK when sign up is valid", (done) => {
     request(app)
@@ -24,5 +35,39 @@ describe("user sign up", () => {
       })
       .expect((response) => expect(response.body.message).toBe("User created"))
       .end(done)
+  })
+
+  it("should save the user to the database", (done) => {
+    request(app)
+      .post("/sign-up")
+      .send({
+        username: "LudwigWittgenstein",
+        email: "ludwig@wittgenstein.com",
+        password: "russellisawesome",
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          expect(userList.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  it("should save the username and email to the database", (done) => {
+    request(app)
+      .post("/sign-up")
+      .send({
+        username: "LudwigWittgenstein",
+        email: "ludwig@wittgenstein.com",
+        password: "russellisawesome",
+      })
+      .then(() => {
+        User.findAll().then((userList) => {
+          const savedUser = userList[0]
+          expect(savedUser.username).toBe("LudwigWittgenstein")
+          expect(savedUser.email).toBe("ludwig@wittgenstein.com")
+          done()
+        })
+      })
   })
 })
