b898a744369f1a42d07b504c9258e482c40ef758
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-19 06:08:25 -0300

    feat: if username is falsy, return 400

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,12 @@ const userService = require("../services/user.service")
 const router = express.Router()
 
 router.post("/sign-up", async (req, res) => {
+  const user = req.body
+
+  if (!user.username) {
+    return res.status(400).send()
+  }
+
   await userService.save(req)
   return res.status(200).send({ message: "User created" })
 })
