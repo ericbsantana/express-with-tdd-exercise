bf399774fc4666bd857c9189b36e7d4bbba1815a
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-19 06:22:05 -0300

    refactor: create middleware to validate username

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const userService = require("../services/user.service")
 const router = express.Router()
 
-router.post("/sign-up", async (req, res) => {
+const validateUsername = (req, res, next) => {
   const user = req.body
 
   if (!user.username) {
@@ -13,6 +13,10 @@ router.post("/sign-up", async (req, res) => {
     })
   }
 
+  next()
+}
+
+router.post("/sign-up", validateUsername, async (req, res) => {
   await userService.save(req)
   return res.status(200).send({ message: "User created" })
 })
