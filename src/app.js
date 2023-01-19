e634416153c90b8b87aee8bbe1da7fbeee2ad68b
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-19 05:21:59 -0300

    feat: make sign-up route to create user

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require("express")
-
+const User = require("./models/User")
 const app = express()
 
+app.use(express.json())
+
 app.post("/sign-up", (req, res) => {
-  return res.status(200).send({ message: "User created" })
+  User.create(req.body).then(() => {
+    return res.status(200).send({ message: "User created" })
+  })
 })
 
 module.exports = app
