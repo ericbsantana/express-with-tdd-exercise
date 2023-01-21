8d64c8fb2c34a5e458f305c253c67339d04ead6f
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-21 00:08:53 -0300

    feat: filter fields from body to create user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,10 +2,12 @@ const User = require("../models/User")
 const bcrypt = require("bcrypt")
 
 const save = async (req) => {
+  const { username, email } = req.body
   const hash = await bcrypt.hash(req.body.password, 8)
 
   const user = {
-    ...req.body,
+    username,
+    email,
     password: hash,
   }
 
