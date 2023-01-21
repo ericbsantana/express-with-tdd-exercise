3dee60274e607a71cd0cb59c24f5154d57254ef6
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-21 01:35:30 -0300

    feat: create a transaction to stop saving user when email is not sent

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,10 +2,12 @@ const User = require("../models/User")
 const bcrypt = require("bcrypt")
 const EmailService = require("./email.service")
 const { v4: uuid } = require("uuid")
+const sequelize = require("../config/database")
 
 const save = async (req) => {
   const { username, email } = req.body
   const hash = await bcrypt.hash(req.body.password, 8)
+  const transaction = await sequelize.transaction()
 
   const user = {
     username,
@@ -14,11 +16,13 @@ const save = async (req) => {
     token: uuid(),
   }
 
-  const createdUser = await User.create(user)
+  const createdUser = await User.create(user, { transaction })
   const { token } = await createdUser.toJSON()
   try {
     await EmailService.sendActivateAccountEmail({ to: email, token })
+    await transaction.commit()
   } catch (err) {
+    await transaction.rollback()
     throw new Error("Failed to send e-mail")
   }
 }
