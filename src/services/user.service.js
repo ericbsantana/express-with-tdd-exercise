9eeceaa17dfd6ec2543bdb852e957b77b994f493
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-21 00:29:11 -0300

    feat: send email with token to user

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,5 +1,7 @@
 const User = require("../models/User")
 const bcrypt = require("bcrypt")
+const nodemailer = require("nodemailer")
+const nodemailerStub = require("nodemailer-stub")
 
 const save = async (req) => {
   const { username, email } = req.body
@@ -11,7 +13,17 @@ const save = async (req) => {
     password: hash,
   }
 
-  await User.create(user)
+  const createdUser = await User.create(user)
+  const { token } = await createdUser.toJSON()
+
+  const transporter = nodemailer.createTransport(nodemailerStub.stubTransport)
+
+  await transporter.sendMail({
+    from: "Express With TDD <express@tdd.com>",
+    to: email,
+    subject: "Activate your account",
+    html: `Your token is ${token}`,
+  })
 }
 
 const findOne = async (fields) => {
