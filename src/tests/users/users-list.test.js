c756b7ba6cab43649b6ebacaf46cc4f5d5795a11
Eric Bitencourt de Santana <bitencourteric@hotmail.com>
2023-01-21 14:14:46 -0300

    test: should return 10 users when there 11 on database

--- src/tests/users/users-list.test.js
+++ src/tests/users/users-list.test.js
@@ -1,6 +1,17 @@
 const request = require("supertest")
 const app = require("../../app")
 
+const User = require("../../models/User")
+const sequelize = require("../../config/database")
+
+beforeAll(async () => {
+  return sequelize.sync()
+})
+
+beforeEach(() => {
+  return User.destroy({ truncate: true })
+})
+
 describe("User listing", () => {
   describe("GET /users", () => {
     it("should return 200 if no user is on database", async () => {
@@ -16,6 +27,18 @@ describe("User listing", () => {
         size: 10,
         totalPages: 0,
       })
+    })
+
+    it("should return 10 users in page content when there are 11 users in database", async () => {
+      for (let i = 0; i < 11; i++) {
+        await User.create({
+          username: `philosopher${i}`,
+          email: `philosopher${i}@phi.com`,
+          password: `ilovemathtoo`,
+        })
+      }
+      const response = await request(app).get("/users")
+      expect(response.body.data.length).toBe(10)
     })
   })
 })
